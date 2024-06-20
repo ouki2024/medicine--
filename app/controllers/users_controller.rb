3eabdfca1b9fc432d993201419d311227856d5d0
EC2 Default User <ec2-user@ip-172-31-10-136.ap-northeast-1.compute.internal>
2024-06-20 09:08:36 +0000

    [add]mypage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,34 @@
 class UsersController < ApplicationController
   
   def show
-    @nickname = current_user.nickname
+    @user = current_user
   
   end
   
+  def edit
+    @user = User.find(params[:id])
+    if @user == current_user
+      render :edit
+      return
+    end
+    redirect_to user_path(current_user)
+  end
   
+  def update
+    @user = User.find(params[:id])
+    
+    if @user.update(user_params)
+     redirect_to user_path(current_user)
+    else
+     render :edit   
+    end
+  end
+  
+  
+  private
+  def user_params
+        
+  params.require(:user).permit(:nickname, :profile_image, :introduction)  
+  end
+    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,16 +5,18 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   validates :nickname, uniqueness: true 
+  validates :nickname, presence: true, length: { minimum: 1, maximum: 20}, uniqueness: true
   
   
   has_one_attached :profile_image
   
-  def get_profile_image
+  def get_profile_image(width, height)
+    
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    profile_image.variant(resize_to_limit: [width, height]).processed
+      profile_image.variant(resize_to_limit: [width, height]).processed
   end
   
          
