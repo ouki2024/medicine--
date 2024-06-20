462dda1b6d812981dc2e9072b025b7660435c5d4
EC2 Default User <ec2-user@ip-172-31-10-136.ap-northeast-1.compute.internal>
2024-06-20 06:53:08 +0000

    [add]ActiveStorage

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-  validates :nickname, uniqueness: true       
+  validates :nickname, uniqueness: true 
+  
+  
+  has_one_attached :profile_image
+  
+  def get_profile_image
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+  
          
 end
