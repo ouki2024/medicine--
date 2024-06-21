5d7d5b4c855905af8889fe451cdde0b15ceddc62
EC2 Default User <ec2-user@ip-172-31-10-136.ap-northeast-1.compute.internal>
2024-06-21 12:45:51 +0000

    [add]review・controller,view作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/show'
+
   devise_for :users
   root to:'homes#top'
   get '/about' => 'homes#about' 
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   devise_scope :user do
     post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
   end
+  
+  resources :reviews
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
