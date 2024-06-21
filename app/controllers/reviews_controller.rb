fa3c6a8762312ecc73b5a5f62dbd69077f20cdb4
EC2 Default User <ec2-user@ip-172-31-10-136.ap-northeast-1.compute.internal>
2024-06-21 13:03:06 +0000

    [add]review,controller記述

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,17 @@
 class ReviewsController < ApplicationController
   def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.user_id = current_user.id
+    @review.save
+    flash[:notice]="投稿に成功しました！"
+    redirect_to review_path
+  end
+
+  def index
   end
 
   def show
@@ -7,7 +19,20 @@ class ReviewsController < ApplicationController
 
   def edit
   end
-
-  def index
+  
+  def update
+    
   end
+
+  def destroy
+    
+  end
+  
+  # 投稿データのストロングパラメータ
+  private
+
+  def review_params
+    params.require(:review).permit(:gender, :hospital, :clinical_department, :pharmacy, :use_count, :adress, :age, :visit_month, :cost, :content)
+  end
+ 
 end
