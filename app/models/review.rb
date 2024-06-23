4593b93ce249170842629888db92bd6b86e66616
EC2 Default User <ec2-user@ip-172-31-10-136.ap-northeast-1.compute.internal>
2024-06-23 13:17:08 +0000

    [add]投稿機能・バリデーション設定

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,28 @@ class Review < ApplicationRecord
     
     belongs_to :user
     
+    has_one_attached :profile_image  
+    
+    validates :nickname, presence: true
+    validates :gender, presence: true
+    validates :age, presence: true
+    validates :address, presence: true
+    validates :hospital, presence: true
+    validates :clinical_department, presence: true
+    validates :pharmacy, presence: true
+    validates :use_count, presence: true
+    validates :visit_month, presence: true
+    
+    validates :content, presence: true, length: { minimum: 5, maximum: 200}
+    
+    def get_profile_image(width, height)
+    
+        unless profile_image.attached?
+            file_path = Rails.root.join('app/assets/images/no_image.jpg')
+            profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+        
+        end
+            profile_image.variant(resize_to_limit: [width, height]).processed
+    end
+    
 end
