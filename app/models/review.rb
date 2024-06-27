a746d1c7a09bc849edd8fc7031f400ab73dc3bf4
EC2 Default User <ec2-user@ip-172-31-10-136.ap-northeast-1.compute.internal>
2024-06-27 03:37:40 +0000

    [add]tag/review バリレーション

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,9 @@ class Review < ApplicationRecord
     
     belongs_to :user
     has_many :review_comments, dependent: :destroy
+    has_many  :tag_relationships, dependent: :destroy
+        has_many  :tags, through: :tag_relationships
+
     
     has_one_attached :profile_image  
     
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,7 @@
 class Tag < ApplicationRecord
+    
+    has_many   :tag_relationships, dependent: :destroy
+    has_many   :reviews, through: :tag_relationships
+    validates :name, uniqueness: true
+    
 end
