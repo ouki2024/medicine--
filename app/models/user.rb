bdb5d76097f0678755cf8f778090502b9665d844
EC2 Default User <ec2-user@ip-172-31-23-188.ap-northeast-1.compute.internal>
2024-07-12 12:50:33 +0000

    [add]composition/public

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,25 +2,25 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, 
+         :recoverable, :rememberable, :validatable,
          :authentication_keys => [:nickname]
-  validates :nickname, presence: true, length: { minimum: 1, maximum: 20}, uniqueness: true 
- 
-  
+  validates :nickname, presence: true, length: { minimum: 1, maximum: 20}, uniqueness: true
+
+
   has_many :reviews, dependent: :destroy
   has_many :review_comments, dependent: :destroy
   has_one_attached :profile_image
-  
+
   def get_profile_image(width, height)
-    
+
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
       profile_image.variant(resize_to_limit: [width, height]).processed
   end
-  
-  
+
+
   GUEST_USER_EMAIL = "guest@example.com"
 
   def self.guest
@@ -29,10 +29,10 @@ class User < ApplicationRecord
       user.nickname = "guestuser"
     end
   end
-  
+
   def guest_user?
     email == GUEST_USER_EMAIL
   end
-  
-         
+
+
 end
