af7c223fdc9f83d3b8115b1f58350353b03c8eb7
EC2 Default User <ec2-user@ip-172-31-23-188.ap-northeast-1.compute.internal>
2024-07-14 09:41:36 +0000

    [add]seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,113 @@ Admin.create!(
    email: 'admin@admin',
    password: 'password'
 )
+
+users_params = [
+    {
+      id: 1,
+      email: "test1@test.com",
+      password: "password",
+      nickname: "test1"
+    },
+    {
+      id: 2,
+      email: "test2@test.com",
+      password: "password",
+      nickname: "test2"
+    },
+    {
+      id: 3,
+      email: "test3@test.com",
+      password: "password",
+      nickname: "test3"
+    },
+  ]
+
+users_params.each do |user_params|
+  User.create!(user_params)
+end
+
+tag_names = %w(
+    薬局で受け取り可
+    院内処方可
+    複数薬局あり
+    駅近
+  )
+
+tag_names.each do |name|
+  Tag.create!(name: name)
+end
+
+reviews_params = [
+    {
+      user_id: 1,
+      nickname: "aki",
+      gender: "男",
+      hospital: "秋病院",
+      clinical_department: "内科",
+      pharmacy: "あき薬局",
+      use_count: "初めて",
+      address: "神奈川県",
+      age: 20,
+      visit_month: 7,
+      cost: 1500,
+      content: "病院内の雰囲気もよく過ごしやすかったです。"
+    },
+    {
+      user_id: 2,
+      nickname: "huyu",
+      gender: "男",
+      hospital: "冬病院",
+      clinical_department: "内科",
+      pharmacy: "ふゆ薬局",
+      use_count: "初めて",
+      address: "神奈川県",
+      age: 20,
+      visit_month: 7,
+      cost: 1500,
+      content: "病院内の雰囲気もよく過ごしやすかったです。"
+    },
+    {
+      user_id: 3,
+      nickname: "haru",
+      gender: "男",
+      hospital: "春病院",
+      clinical_department: "内科",
+      pharmacy: "はる薬局",
+      use_count: "初めて",
+      address: "神奈川県",
+      age: 20,
+      visit_month: 7,
+      cost: 1500,
+      content: "病院内の雰囲気もよく過ごしやすかったです。"
+    },
+    {
+      user_id: 3,
+      nickname: "haru",
+      gender: "男",
+      hospital: "春病院",
+      clinical_department: "内科",
+      pharmacy: "はる薬局",
+      use_count: "初めて",
+      address: "神奈川県",
+      age: 20,
+      visit_month: 7,
+      cost: 1500,
+      content: "病院内の雰囲気もよく過ごしやすかったです。"
+    }
+  ]
+
+tags = Tag.all
+
+reviews_params.each do |review_params|
+  review = Review.create!(review_params)
+  (tags.pluck(:id)).sample(rand(0..3)).each do |tag_id|
+    review.tag_relationships.create!(tag_id: tag_id)
+  end
+end
+
+
+
+if Rails.env.development?
+   #開発環境のみで稼働する場所
+end
